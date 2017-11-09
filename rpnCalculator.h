9afeb232ee3507fdea62e4910b1799e14ac98e13
nwillhite <nwill016@ucr.edu>
2017-11-08 19:00:12 -0800

    added getAnswer() and working on calculate function

--- rpnCalculator.h
+++ rpnCalculator.h
@@ -18,7 +18,11 @@ class RPN : public Stack<type> {
     vector<string> token;
 
   public:
-    RPN();
+    RPN
+
+    void calculate();
+    void removeSpace(string &str);
+    type getAnswer();
 
 };
 
@@ -27,5 +31,45 @@ template <class type>
 RPN<type>::RPN() {
   input = "";
 }
+
+
+template <class type>
+void RPN<type>::calculate() {
+
+  for (int i = 0; i < token.size(); i++) {
+    if(token[i] != "*" || token[i] != "/" || token[i] != "+" || token[i] != "-") {
+      this->push(token[i]);
+    }
+    else {
+      type tmp2 = this->peek();
+      this->pop();
+      type tmp1 = this->peek();
+      this->pop();
+    }
+    if(token[i] == "*") {
+      this->push(std::to_string(atof(tmp1.c_str()) * atof(tmp2.c_str())));
+    }
+    
+  }
+
+}
+
+
+template <class type>
+void RPN<type>::removeSpace(string &str) {
+
+  for (int i = 0; i < str.length(); i++) {
+
+    if(str[i] == ' ' && str[i+1] == ' ') {
+      str.erase(i,1);
+    }
+  }
+}
+
+template <class type>
+type RPN<type>::getAnswer() {
+  return this->peek();
+}
+
 
 #endif
